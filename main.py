e01adad2f364534a08c4fcb9da19c669ef0170c6
stalker-john <hussainmarufbhilai@gmail.com>
2021-11-29 20:06:39 +0530

    edited main and spider

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ from spider import *
 
 max_articles = 10
 
-x = fetch_links('https://ieeexplore.ieee.org/document/8301529/references')
+doc_link = "https://ieeexplore.ieee.org/document/8301529"
+
+x = fetch_links(doc_link)
 
 for y in x:
     print(y)
