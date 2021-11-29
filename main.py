853b40d4886c4e31fe8b302d19fb05ee0d1cdfb9
stalker-john <hussainmarufbhilai@gmail.com>
2021-11-29 20:09:38 +0530

    changes to main

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 from spider import *
 
-max_articles = 10
-
 doc_link = "https://ieeexplore.ieee.org/document/8301529"
 
-x = fetch_links(doc_link)
+papers = fetch_links(doc_link)
 
-for y in x:
-    print(y)
+for link in papers:
+    print(link)
